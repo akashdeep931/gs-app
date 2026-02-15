36c84e835dd01aecd190fec35b498f9c254785ad
akashdeep931 <akashdeepsingh931@gmail.com>
2026-02-15 17:39:17 +0000

    refactor: general refactoring

--- frontend/src/context/GSServiceProvider.tsx
+++ frontend/src/context/GSServiceProvider.tsx
@@ -3,7 +3,7 @@ import { GSService } from "../services/GSService";
 import { GSServiceContext } from "./GSServiceContext";
 
 export function GSServiceProvider({ children }: { children: ReactNode }) {
-  const service = GSService.getInstance();
+  const service = GSService.getInstance(import.meta.env.VITE_GS_API_URL);
 
   return (
     <GSServiceContext.Provider value={service}>
